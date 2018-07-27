bd8359e1b08a96542620f1101147a266a7af2179
DyadkinMaxim <40363737+DyadkinMaxim@users.noreply.github.com>
2018-07-27 14:43:55 +0300

    Add files via upload

---
+++ GameCanvas.java
@@ -0,0 +1,44 @@
+package neuroleptic;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameCanvas extends JPanel {
+
+    private MainWindow mainWindow;
+    private long lastFrameTime;
+    public static int counter = 0; //счетчик для изменения цвета
+
+    GameCanvas(MainWindow mainWindow) {
+        this.mainWindow = mainWindow;
+        lastFrameTime = System.nanoTime();
+        setBackground(BackgroundColor.getColor());
+        counter++;
+        if(counter == 25) counter = 0;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        long currentTime = System.nanoTime();
+        float deltaTime = (currentTime - lastFrameTime) * 0.000000001f;
+        lastFrameTime = currentTime;
+
+        // friday magic
+        try {
+            Thread.sleep(16);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        mainWindow.onDrawFrame(this, g, deltaTime);
+
+        repaint();
+    }
+
+    public int getLeft() { return 0; }
+    public int getRight() { return getWidth() - 1; }
+    public int getTop() { return 0; }
+    public int getBottom() { return getHeight() - 1; }
+}
